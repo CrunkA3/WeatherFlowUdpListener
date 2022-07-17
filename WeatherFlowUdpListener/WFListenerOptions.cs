07577ec0f5508398b33d1bbcb2921f0dd68243ed
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2022-07-17 19:42:07 +0200

    make listenPort public

--- WeatherFlowUdpListener/WFListenerOptions.cs
+++ WeatherFlowUdpListener/WFListenerOptions.cs
@@ -3,7 +3,7 @@ using System.Net.Sockets;
 
 public class WFListenerOptions
 {
-    const int listenPort = 50222;
+    public const int listenPort = 50222;
     public UdpClient Client { get; set; } = new UdpClient(listenPort);
     public IPEndPoint EndPoint { get; set; } = new IPEndPoint(IPAddress.Any, listenPort);
 }
