59b6e72cab3ba8af11ed150f5442e2c682990088
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2022-07-19 18:37:44 +0200

    add showcase app

---
+++ WeatherFlowUdpListener.Showcase/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.WriteLine("Initialize Listener");
+
+CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+await WeatherFlowUdpListener.WFListener
+    .Create()
+    .OnReceiveObservationAirMessage(msg =>
+    {
+        Console.WriteLine($"Air-Temperature: {msg.AirTemperature}");
+        cancellationTokenSource.Cancel();
+    })
+    .OnReceiveObservationSkyMessage(msg =>
+    {
+        Console.WriteLine($"UV-Index: {msg.UV}");
+        cancellationTokenSource.Cancel();
+    })
+    .OnReceiveRapidWindMessage(msg =>
+    {
+        Console.WriteLine($"Wind-Speed: {msg.WindSpeed}");
+        cancellationTokenSource.Cancel();
+    })
+    .ListenAsync(cancellationToken);
